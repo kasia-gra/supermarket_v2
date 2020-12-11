cb128576e3ba880e78c6067e264a4e24ba41458d
kasia-gra <grabowska.kat@gmail.com>
2020-12-11 12:57:49 +0100

    Added files examples with codecool data

--- src/main/java/supermarket/Main.java
+++ src/main/java/supermarket/Main.java
@@ -9,7 +9,8 @@ public class Main {
         FileHandler fh = new FileHandler();
         List<Integer> bill1  = fh.readBill("bill1.txt");
         List<Integer> bill2  = fh.readBill("bill2.txt");
-        Supermarket supermarket = new Supermarket(fh, "prices.csv");
+        List<Integer> bill3  = fh.readBill("bill3.txt");
+        Supermarket supermarket = new Supermarket(fh, "prices2.csv");
         System.out.println("Total price is: " +supermarket.getTotalPrice(bill1));
     }
 
